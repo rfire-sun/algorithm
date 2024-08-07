dc646eb74b4673f3911c47835239b4b3325db741
cheng sun <suncheng@genomics.cn>
2024-08-07 16:26:35 +0800

    exec-主要加入使用loop的方法

--- src/main/java/org/example/algorithm1/PowTest.java
+++ src/main/java/org/example/algorithm1/PowTest.java
@@ -17,27 +17,50 @@ public class PowTest {
     @Test
     public void testPowers() {
 
-        System.out.println(myPow(2.00, 10));
+//        System.out.println(myPow(2.00, 10));
+        System.out.println(myPow2(2.00, 38));
     }
 
 
     public double myPow(double x, int n) {
         if (x == 1 || x == 0) return x;
 
-        return n < 0 ? 1/quickMul(x, -n) : quickMul(x, n);
+        return n < 0 ? 1 / quickMul(x, -n) : quickMul(x, n);
 
     }
 
 
-    public double quickMul(double x, long N) {
-        if (N == 0) {
+    public double quickMul(double x, long n) {
+        if (n == 0) {
             return 1;
         }
 
-        double y = quickMul(x, N / 2);
+        double y = quickMul(x, n / 2);
 
 
-        return N % 2 == 0 ? y * y : y * y * x;
+        return n % 2 == 0 ? y * y : y * y * x;
+    }
+
+
+    public double myPow2(double x, int n) {
+        if (x == 0 || x == 1) return x;
+
+        double rs = 1;
+        // 这里注意使用了long
+        long nx = n;
+        nx = nx < 0 ? -nx : nx;
+
+        // 这个看起来只要乘以相应数量的x就行了
+        while (nx > 0) {
+            if (nx % 2 == 1) {
+                rs = rs * x;
+            }
+
+            x *= x;
+            nx = nx / 2;
+        }
+
+        return n < 0 ? 1 / rs : rs;
     }
 
 
